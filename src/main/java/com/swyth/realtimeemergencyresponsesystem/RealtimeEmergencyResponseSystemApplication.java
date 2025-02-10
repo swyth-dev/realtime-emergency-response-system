b65af94db9cd95c8c829c5f6f91682a9812d806a
swyth_ <swyth+github@pm.me>
2025-02-10 11:41:45 +0000

    chore: init project

---
+++ src/main/java/com/swyth/realtimeemergencyresponsesystem/RealtimeEmergencyResponseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.swyth.realtimeemergencyresponsesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeEmergencyResponseSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeEmergencyResponseSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swyth/realtimeemergencyresponsesystem/RealtimeEmergencyResponseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyth.realtimeemergencyresponsesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeEmergencyResponseSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
