19981a14ed70c8561ac2ded5f94ced195a7c1471
swyth_ <swyth+github@pm.me>
2025-02-10 15:16:02 +0000

    chore: update bed availability service

---
+++ bed-availability-service/src/main/java/com/swyth/bedavailabilityservice/BedAvailabilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.swyth.bedavailabilityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedAvailabilityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BedAvailabilityServiceApplication.class, args);
+    }
+
+}
