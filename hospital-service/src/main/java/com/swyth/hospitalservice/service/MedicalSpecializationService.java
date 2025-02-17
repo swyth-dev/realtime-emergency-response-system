7142ad6d34b20fb85cbe7130ddd76e52d2574e69
swyth_ <swyth+github@pm.me>
2025-02-17 17:40:37 +0000

    feat: getting work done on fetching medical specializations

---
+++ hospital-service/src/main/java/com/swyth/hospitalservice/service/MedicalSpecializationService.java
@@ -0,0 +1,23 @@
+package com.swyth.hospitalservice.service;
+
+import com.swyth.hospitalservice.entity.MedicalSpecialization;
+import com.swyth.hospitalservice.repository.MedicalSpecializationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MedicalSpecializationService {
+
+    private final MedicalSpecializationRepository medicalSpecializationRepository;
+
+    public MedicalSpecializationService(MedicalSpecializationRepository medicalSpecializationRepository) {
+        this.medicalSpecializationRepository = medicalSpecializationRepository;
+    }
+
+    public List<MedicalSpecialization> findAll() {
+        List<MedicalSpecialization> medicalSpecializations = new ArrayList<>(medicalSpecializationRepository.findAll());
+        return medicalSpecializations;
+    }
+}
