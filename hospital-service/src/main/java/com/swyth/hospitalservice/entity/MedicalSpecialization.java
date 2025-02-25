fb2ae63dfb50ef35b755fc2141c14e6ec61c305f
swyth_ <swyth+github@pm.me>
2025-02-25 10:52:26 +0000

    refacto: add MedicalSpecialization ManyToMany relation to respect domain

--- hospital-service/src/main/java/com/swyth/hospitalservice/entity/MedicalSpecialization.java
+++ hospital-service/src/main/java/com/swyth/hospitalservice/entity/MedicalSpecialization.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Getter
 @NoArgsConstructor
@@ -20,4 +23,8 @@ public class MedicalSpecialization {
     @Column(nullable = false, name = "specialization_group")
     @JsonProperty("specialization_group")
     private String specializationGroup;
+
+    @ManyToMany(mappedBy = "specializations", fetch = FetchType.EAGER) // Referencing Student's ManyToMany field
+    private Set<Hospital> hospitals = new HashSet<>();
+    // TODO: check https://www.baeldung.com/jpa-many-to-many to add an additional field to relation jointure table
 }
