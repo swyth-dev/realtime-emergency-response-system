2bb239fc2b7f34c95e4a1f6d51e3c01ae7e958e8
swyth_ <swyth+github@pm.me>
2025-03-17 12:35:43 +0100

    chore: init service

---
+++ emergency-service/src/main/java/com/swyth/emergencyservice/EmergencyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.swyth.emergencyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyServiceApplication.class, args);
+	}
+
+}
---
+++ emergency-service/src/test/java/com/swyth/emergencyservice/EmergencyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyth.emergencyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
