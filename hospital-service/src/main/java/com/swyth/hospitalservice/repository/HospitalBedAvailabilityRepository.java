0aa37599dbeecbc1c819bb8fcc380a1ae6dec645
swyth_ <swyth+github@pm.me>
2025-03-25 14:50:04 +0100

    feat: implement method & logic to consume message received from messaging broker

--- hospital-service/src/main/java/com/swyth/hospitalservice/repository/HospitalBedAvailabilityRepository.java
+++ hospital-service/src/main/java/com/swyth/hospitalservice/repository/HospitalBedAvailabilityRepository.java
@@ -4,5 +4,9 @@ import com.swyth.hospitalservice.entity.HospitalBedAvailability;
 import com.swyth.hospitalservice.entity.HospitalBedAvailabilityId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HospitalBedAvailabilityRepository extends JpaRepository<HospitalBedAvailability, HospitalBedAvailabilityId> {
+    // Find an entry by hospitalId and medicalSpecializationId
+    Optional<HospitalBedAvailability> findByHospitalIdAndSpecializationId(Long hospitalId, Long medicalSpecializationId);
 }
