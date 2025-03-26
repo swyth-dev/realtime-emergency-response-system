6dcc972da039684ebaf6ba5d6d8ae80b01643243
swyth_ <swyth+github@pm.me>
2025-03-26 18:29:53 +0100

    refacto: update config

--- frontend/realtime-emergency-app/src/app/app.config.ts
+++ frontend/realtime-emergency-app/src/app/app.config.ts
@@ -2,12 +2,15 @@ import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {HttpErrorInterceptor} from './services/http-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
     provideZoneChangeDetection({ eventCoalescing: true }), // Optimize Angular's change detection
     provideRouter(routes), // Application routes
-    provideHttpClient() // HTTP client for API communication
+    provideHttpClient(withInterceptors([HttpErrorInterceptor])) // HTTP client for API communication
   ]
 };
