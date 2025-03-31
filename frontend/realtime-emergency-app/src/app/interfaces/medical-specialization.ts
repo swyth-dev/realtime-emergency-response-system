b81fc8b80da32e9487cd2b680c361c20fc5c87f6
swyth_ <swyth+github@pm.me>
2025-03-31 23:44:27 +0200

    refacto: update interfaces to decouple both

--- frontend/realtime-emergency-app/src/app/interfaces/medical-specialization.ts
+++ frontend/realtime-emergency-app/src/app/interfaces/medical-specialization.ts
@@ -1,8 +1,17 @@
-import {Hospital} from './hospital';
-
 export interface MedicalSpecialization {
   id: number | null;
   name: string;
   group: string;
-  hospitals: Hospital[];
+  hospitals: HospitalAvailability[];
+}
+
+interface HospitalAvailability {
+  id: number;
+  name: string;
+  address: string;
+  postCode: string;
+  city: string;
+  latitude: number;
+  longitude: number;
+  bedsAvailable: number;
 }
