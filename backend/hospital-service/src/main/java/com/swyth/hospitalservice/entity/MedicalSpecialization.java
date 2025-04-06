178d2fac79e699a67fad60c70dcc5107c225b0a7
swyth_ <swyth+github@pm.me>
2025-04-06 13:36:33 +0200

    refacto: imports

--- backend/hospital-service/src/main/java/com/swyth/hospitalservice/entity/MedicalSpecialization.java
+++ backend/hospital-service/src/main/java/com/swyth/hospitalservice/entity/MedicalSpecialization.java
@@ -1,10 +1,7 @@
 package com.swyth.hospitalservice.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
